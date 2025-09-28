01e9b57fb4bdbff657ee7e6d821c7be4063ff2e5
Mr_Sekiro <abdullah811001@gmail.com>
2025-09-28 19:43:58 +0300

    Generic Repository & Unit of Work

---
+++ DataAccess/Data/Migrations/20250928164020_Update-BaseEntity.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "TimeLogs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Projects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "TimeLogs");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Clients");
+        }
+    }
+}
---
+++ DataAccess/Repositories/Classes/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Data;
+using DataAccess.Models.Shared;
+using DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataAccess.Repositories.Classes
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        //=============== Lazy Implementation ================//
+        private readonly ApplicationDbContext dbContext;
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public int SaveChanges()
+        {
+            foreach (var entry in dbContext.ChangeTracker.Entries<BaseEntity>())
+            {
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+                    entry.Entity.DateModified = DateTime.Now;
+            }
+
+            return dbContext.SaveChanges();
+        }
+
+        public void Dispose() => dbContext.Dispose();
+    }
+}
