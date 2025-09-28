edd60155b4e3b8cab911fa6cdab8bafec5bea727
Mr_Sekiro <abdullah811001@gmail.com>
2025-09-28 20:27:47 +0300

    Project Repository

---
+++ DataAccess/Repositories/Classes/ProjectRepo.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Data;
+using DataAccess.Models;
+using DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Classes
+{
+    internal class ProjectRepo(ApplicationDbContext dbContext) : GenericRepo<Project>(dbContext), IProjectRepo
+    {
+
+    }
+}
